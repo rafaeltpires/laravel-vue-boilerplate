138d18acd8e090697ae4d24ec193e5b1fb90ba07
rafaeltpires <me@rafaeltpires.dev>
2023-06-07 20:53:06 +0100

    add boilerplate

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <span class="text-3xl font-bold text-red-400 hover:text-red-600">Lets GOOOOOOOOOOOOOOO</span>
+
+</template>
+
+<style scoped>
+
+</style>
